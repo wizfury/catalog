7ee1bd33605edb6689b77d4fd64cc96a3dd8d325
wizfury <Aakarsh135@gmail.com>
2022-08-06 14:20:18 +0530

    making login oage

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,42 +1,57 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class loginpage extends StatelessWidget {
-  const loginpage({ Key? key }) : super(key: key);
+  const loginpage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            "assets/images/login.png",
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Welcome",
-            style: newMethod(),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          TextFormField(
-              decoration: InputDecoration(hintText: "Enter the username",labelText: "Username"),
-          )
-          
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Welcome",
+              style: newMethod(),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Enter the username",
+                labelText: "Username",
+              ),
+            ),
+            TextFormField(
+              decoration:
+                  InputDecoration(hintText: "Password", labelText: "Password"),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                print("Hello world!");
+              },
+              child: Text("Login"),
+              style: TextButton.styleFrom(),
+            )
+          ],
         ),
+      ),
     );
   }
 
   TextStyle newMethod() {
     return TextStyle(
-            fontSize: 22,
-            color: Colors.black,
-            fontWeight: FontWeight.bold
-          );
+        fontSize: 22, color: Colors.black, fontWeight: FontWeight.bold);
   }
 }
