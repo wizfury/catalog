da3b2ffd0bf36c1fc7c6f4a9cf9685c5f925c34c
wizfury <Aakarsh135@gmail.com>
2022-08-08 22:30:27 +0530

    adding aniamtion to login button

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -3,9 +3,16 @@ import 'dart:html';
 import 'package:catalog/util/routes.dart';
 import 'package:flutter/material.dart';
 
-class loginpage extends StatelessWidget {
+class loginpage extends StatefulWidget {
   const loginpage({Key? key}) : super(key: key);
 
+  @override
+  State<loginpage> createState() => _loginpageState();
+}
+
+class _loginpageState extends State<loginpage> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -17,13 +24,12 @@ class loginpage extends StatelessWidget {
               Image.asset(
                 "assets/images/login.png",
                 fit: BoxFit.cover,
-                
               ),
               SizedBox(
                 height: 20,
               ),
               Text(
-                "Welcome",
+                "Welcome, $name",
                 style: newMethod(),
               ),
               SizedBox(
@@ -34,20 +40,48 @@ class loginpage extends StatelessWidget {
                   hintText: "Enter the username",
                   labelText: "Username",
                 ),
+                onChanged: (value) {
+                  name = value;
+                  setState(() {});
+                },
               ),
               TextFormField(
-                decoration:
-                    InputDecoration(hintText: "Password", labelText: "Password"),
+                decoration: InputDecoration(
+                    hintText: "Password", labelText: "Password"),
               ),
               SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, myroutes.homeroute);
+
+              InkWell(
+                onTap: () {
+                  //Navigator.pushNamed(context, myroutes.homeroute);
+                  setState(() {
+                    changeButton = true;
+                  });
                 },
-                child: Text("Login"),
-                style: TextButton.styleFrom(minimumSize: Size(120,40)),
-                
+                child: AnimatedContainer(
+                  duration: Duration(seconds: 1),
+                  width: changeButton?50:150,
+                  height: 50,
+                  alignment: Alignment.center,
+                  child: changeButton?Icon(Icons.done):Text(
+                    "Login",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                  decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      borderRadius: BorderRadius.circular(changeButton?20:8)),
+                ),
               )
+              // ElevatedButton(
+              //   onPressed: () {
+              //     Navigator.pushNamed(context, myroutes.homeroute);
+              //   },
+              //   child: Text("Login"),
+              //   style: TextButton.styleFrom(minimumSize: Size(120, 40)),
+              // )
             ],
           ),
         ),
